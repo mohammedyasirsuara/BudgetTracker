0f73872e475217ef068b447e065c0aef6c760fd2
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-10-03 00:51:34 -0400

    deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,74 +1,71 @@
+
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/index.js',
-    '/manifest.webmanifest',
-    '/style.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-192x192.png'
-  ];
-  
-  const CACHE_NAME = 'static-cache-v2';
-  const DATA_CACHE_NAME = 'data-cache-v1';
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(CACHE_NAME)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+  "/",
+  "/db.js",
+  "/index.js",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// Install
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-  
-  //fetch
-  self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-  
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        }).catch(err => console.log(err))
       );
-  
-      return;
-    }
-  
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(event.request)
+          .then((response) => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch((err) => {
+            return cache.match(event.request);
+          });
       })
     );
-  });
+    return;
+  }
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+});
--- server.js
+++ server.js
@@ -14,11 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
